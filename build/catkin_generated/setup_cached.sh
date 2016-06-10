e7092228c9ca38378f1b5846bd96d13ba98881b7
keniso <kyesh@umich.edu>
2016-06-09 23:19:05 -0400

    Made launch file for TurtleSimTeleop

--- build/catkin_generated/setup_cached.sh
+++ build/catkin_generated/setup_cached.sh
@@ -8,11 +8,8 @@
 
 # modified environment variables
 export CMAKE_PREFIX_PATH="/home/keniso/BasicROSWorkspace/devel:$CMAKE_PREFIX_PATH"
-export CPATH="/home/keniso/BasicROSWorkspace/devel/include:$CPATH"
-export LD_LIBRARY_PATH="/home/keniso/BasicROSWorkspace/devel/lib:/home/keniso/BasicROSWorkspace/devel/lib/x86_64-linux-gnu:/opt/ros/indigo/lib/x86_64-linux-gnu:/opt/ros/indigo/lib:/usr/local/lib:/opt/ros/indigo/lib/python2.7/dist-packages"
-export PATH="/home/keniso/BasicROSWorkspace/devel/bin:$PATH"
-export PKG_CONFIG_PATH="/home/keniso/BasicROSWorkspace/devel/lib/pkgconfig:/home/keniso/BasicROSWorkspace/devel/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/x86_64-linux-gnu/pkgconfig:/opt/ros/indigo/lib/pkgconfig"
+export LD_LIBRARY_PATH="/home/keniso/BasicROSWorkspace/devel/lib:/opt/ros/indigo/lib:/usr/local/lib:/opt/ros/indigo/lib/python2.7/dist-packages"
+export PKG_CONFIG_PATH="/home/keniso/BasicROSWorkspace/devel/lib/pkgconfig:$PKG_CONFIG_PATH"
 export PWD="/home/keniso/BasicROSWorkspace/build"
-export PYTHONPATH="/home/keniso/BasicROSWorkspace/devel/lib/python2.7/dist-packages:$PYTHONPATH"
 export ROSLISP_PACKAGE_DIRECTORIES="/home/keniso/BasicROSWorkspace/devel/share/common-lisp"
 export ROS_PACKAGE_PATH="/home/keniso/BasicROSWorkspace/src:$ROS_PACKAGE_PATH"
